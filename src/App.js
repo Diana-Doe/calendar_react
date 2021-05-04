a57b1571ce7613dea9e1a41642b22c76f0ba4997
Diana-Doe <diacha111@gmail.com>
2021-05-04 22:31:27 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Calendar from "./modules/calendar/index";
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div id="logo">
+            <span>
+            </span>
+          </div>
+        </header>
+        <main>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
